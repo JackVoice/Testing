5a02a23cb411efb7772bea49541e37286915ad0c
JackVoice <43949115+JackVoice@users.noreply.github.com>
2018-10-08 12:10:25 +0100

    Create Gradebook.py

---
+++ Code Accdemy/Gradebook.py
@@ -0,0 +1,16 @@
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+subjects = ["Physics", "Calculus", "Poetry", "History"]
+grades = [98,97,85,88]
+
+subjects.append("Computer Science")
+grades.append(100)
+
+gradebook = list(zip(subjects,grades))
+gradebook.append(("Visual arts", 93))
+
+print(gradebook)
+
+full_gradebook = list(gradebook + last_semester_gradebook)
+
+print(full_gradebook)
