169030cd0fd5188a28c8d9f682cdf1006c8637f8
JackVoice <43949115+JackVoice@users.noreply.github.com>
2018-10-08 11:48:49 +0100

    Update Lists.py

--- Code Accdemy/Lists.py
+++ Code Accdemy/Lists.py
@@ -1,2 +1,6 @@
 ints_and_strings = [1, 2, 3, 'four', 'five', "twenty_two"]
 sam_height = ["Sam", 67]
+
+heights = [['Jenny', 61], ['Alexus', 70], ['Sam', 67], ['Grace', 64], ["Vik", 68]]
+
+ages = [["Aaron", 15],["Dhruti", 16]]
