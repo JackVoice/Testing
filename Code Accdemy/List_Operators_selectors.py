a649ba0bae24d913e7eb01008e8afc72da3e0895
JackVoice <43949115+JackVoice@users.noreply.github.com>
2018-10-08 12:16:28 +0100

    Update List_Operators_selectors.py

--- Code Accdemy/List_Operators_selectors.py
+++ Code Accdemy/List_Operators_selectors.py
@@ -2,3 +2,11 @@ employees = ['Michael', 'Dwight', 'Jim', 'Pam', 'Ryan', 'Andy', 'Robert']
 index4 = employees[4]
 print(index4)
 print(len(employees))
+
+
+shopping_list = ['eggs', 'butter', 'milk', 'cucumbers', 'juice', 'cereal']
+print(len(shopping_list))
+last_element = shopping_list[-1]
+print(last_element)
+element5 = shopping_list[5]
+print(element5)
