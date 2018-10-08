e57a026bd90b85fe58850f92cf8d01484c2b73c8
JackVoice <43949115+JackVoice@users.noreply.github.com>
2018-10-08 12:20:12 +0100

    Update List_Operators_selectors.py

--- Code Accdemy/List_Operators_selectors.py
+++ Code Accdemy/List_Operators_selectors.py
@@ -14,7 +14,8 @@ print(element5)
 suitcase = ['shirt', 'shirt', 'pants', 'pants', 'pajamas', 'books']
 
 beginning = suitcase[0:4]
-
 print(beginning)
 
 middle = suitcase[2:4]
+start = suitcase[0:3]
+end = suitcase[4:6]
