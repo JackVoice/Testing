96ede6154360312928ddb0f06046c93b5930d20a
JackVoice <43949115+JackVoice@users.noreply.github.com>
2018-10-08 15:23:48 +0100

    Update Loops_ListComp.py

--- Code Accdemy/Loops_ListComp.py
+++ Code Accdemy/Loops_ListComp.py
@@ -3,3 +3,10 @@ heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
 can_ride_coaster = [height for height in heights if height > 161]
 
 print(can_ride_coaster)
+
+
+celsius = [0, 10, 15, 32, -5, 27, 3]
+
+fahrenheit = [c * 9/5 + 32 for c in celsius]
+
+print(fahrenheit)
