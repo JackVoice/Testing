e1159b0a031127d6f26006a4940164fd196a80f7
JackVoice <43949115+JackVoice@users.noreply.github.com>
2018-10-08 14:54:33 +0100

    Create Lists_Every3.py

---
+++ Code Accdemy/Lists_Every3.py
@@ -0,0 +1,7 @@
+#Write your function here
+def every_three_nums(start):
+  result = list(range(start, 101, 3))
+  return result
+
+#Uncomment the line below when your function is done
+print(every_three_nums(91))
