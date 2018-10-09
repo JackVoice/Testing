78a2e57139e6b24d8560cc48c54965f5a6b21ef9
JackVoice <43949115+JackVoice@users.noreply.github.com>
2018-10-09 12:10:42 +0100

    Create Strings_Format.py

---
+++ Code Accdemy/Strings_Format.py
@@ -0,0 +1,5 @@
+def poem_title_card(poet,title):
+  #Goes backwards for some reason
+  return "The poem \"{}\" is written by {}.".format(title,poet)
+
+print(poem_title_card("Walt Whitman", "I Hear America Singing"))
