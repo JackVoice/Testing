59cd1e20481bd5806519f5eb156ba58807b4bff3
JackVoice <43949115+JackVoice@users.noreply.github.com>
2018-10-11 15:00:06 +0100

    Update Dictionary_MultipleKeys.py

--- Code Accdemy/Dictionary_MultipleKeys.py
+++ Code Accdemy/Dictionary_MultipleKeys.py
@@ -1,4 +1,4 @@
 user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
-user_ids.update({"TheLooper": 138475, "stringQueen": 85739})
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
 #user_ids["TheLooper"] = 138475 does the same thing
 print(user_ids)
