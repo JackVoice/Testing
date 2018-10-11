ffdc669719de77bba6e59df2ed34fb5bfb600e84
JackVoice <43949115+JackVoice@users.noreply.github.com>
2018-10-11 15:02:51 +0100

    Create Dictionary_update.py

---
+++ Code Accdemy/Dictionary_update.py
@@ -0,0 +1,5 @@
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+#adds supporting actress
+oscar_winners["Supporting Actress"] = "Viola Davis"
+#Updates Best picture
+oscar_winners["Best Picture"] = "Moonlight"
